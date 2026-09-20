98c7a84b8e1df85fad9ef45a364ec9a30a14138a
Amarbal7978 <bal985258@gmail.com>
2026-09-20 19:38:32 +0530

    Add complete website

---
+++ API/news.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.NEWS_API_KEY}`
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: "News fetch karne mein problem hui."
+    });
+  }
+}
