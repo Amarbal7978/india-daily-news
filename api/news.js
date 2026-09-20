cc7e1cd7e859a6b2caa1cf896cda2a2c051891fa
Amarbal7978 <bal985258@gmail.com>
2026-09-20 22:23:46 +0530

    Add state and language wise news

--- api/news.js
+++ api/news.js
@@ -5,11 +5,18 @@ const sql = neon(process.env.DATABASE_URL);
 export default async function handler(req, res) {
   try {
     const state = req.query.state || "Odisha";
+    const language = req.query.language || "English";
+const languageCodes = {
+  English: "en",
+  Hindi: "hi",
+  Odia: "en"
+};
 
+const apiLanguage = languageCodes[language] || "en";
     const response = await fetch(
       `https://newsapi.org/v2/everything?q=${encodeURIComponent(
         state + " India"
-      )}&language=en&sortBy=publishedAt&pageSize=10&apiKey=${process.env.NEWS_API_KEY}`
+      )}&language=${apiLanguage}&sortBy=publishedAt&pageSize=10&apiKey=${process.env.NEWS_API_KEY}`
     );
 
     const data = await response.json();
