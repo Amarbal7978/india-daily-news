9a65e5ea1ba3f40d0b56564609a349073dc5ebae
Amarbal7978 <bal985258@gmail.com>
2026-09-21 19:34:48 +0530

    Improve events and culture news filter

--- api/news.js
+++ api/news.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       Education: "India education schools colleges exams",
       Jobs: "India jobs recruitment vacancies government jobs",
       Sports: "India sports cricket football hockey",
-      "Events & Culture": "India festivals events culture entertainment"
+       "Events & Culture": "India traditional festivals cultural celebrations fairs folk dance music theatre"
     };
 
     const searchQuery =
